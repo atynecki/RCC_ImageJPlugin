6ff1f164c8614ad11ab2f0fea8df0063a9f176ae
ArturT <a.tynecki@outlook.com>
2016-04-17 14:49:46 +0200

    add parameters panel, button listener service, image processing inner class

--- RCC.java
+++ RCC.java
@@ -1,19 +1,19 @@
 import ij.*;
 import ij.process.*;
 import ij.gui.*;
+import ij.plugin.frame.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
-import ij.plugin.frame.*;
-
 public class RCC extends PlugInFrame implements ActionListener {	
 	private Panel panel;
-	private int previousID;
 	private static Frame instance;
+	private ImagePlus image = new ImagePlus();
+	
+	private static int frame_width = 400;
+	private static int frame_height = 400;
 	
 	public RCC() {
 		super("Red cell counter");
@@ -25,30 +25,75 @@ public class RCC extends PlugInFrame implements ActionListener {
 		instance = this;
 		addKeyListener(IJ.getInstance());
 		
-		setLayout(new FlowLayout());
-		panel = new Panel();
-		panel.setLayout(new GridLayout(3, 1, 5, 5));
-		addButton("Image load");
-		addButton("Analyze");
-		addButton("Save result");
-		add(panel);
+		/** SET VIEW */
+		this.setSize((int) frame_width,(int) frame_height);
 		
-		pack();
+		setLayout(new FlowLayout());
+		Panel buttonPanel = new Panel();
+		buttonPanel.setLayout(new GridLayout(2, 1, 5, 5));
+		buttonPanel.add(addButton("Analyze"));
+		buttonPanel.add(addButton("Save result"));
+		add(buttonPanel);
+		
+		Panel parametersPanel = new Panel();
+		parametersPanel.setLayout(new GridLayout(2, 1, 5, 5));
+		parametersPanel.add(addParameterField("parameter1", 200, 15));
+		parametersPanel.add(addParameterField("parameter2", 200, 15));
+		add(parametersPanel);
+		
+		/** GET ACTIVE IMAGE */
+		image = IJ.getImage();
+		
 		GUI.center(this);
 		setVisible(true);
 	}
 	
-	void addButton(String label) {
-		Button b = new Button(label);
-		b.addActionListener(this);
-		b.addKeyListener(IJ.getInstance());
-		panel.add(b);
-	}
-
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	Button addButton(String label) {
+		Button button = new Button(label);
+		button.addActionListener(this);
+		button.addKeyListener(IJ.getInstance());
 		
+		return button;
+	}
+	
+	Panel addParameterField(String label, double defaultValue, int digits) {
+		Panel parameterFieldPanel = new Panel();
+		Label labelName = new Label(label);
+		TextField textField = new TextField(digits);
+		
+		parameterFieldPanel.setLayout(new GridBagLayout());
+		GridBagConstraints d = new GridBagConstraints();
+		d.insets = new Insets(5, 10, 5, 5);
+		d.gridx = 0;
+		d.gridy = 0;
+		d.ipady = 15;
+		parameterFieldPanel.add(labelName, d);
+		d.gridx = 1;
+		d.gridy = 0;
+		d.ipady = 15;
+		parameterFieldPanel.add(textField, d);
+		textField.setText(String.valueOf(defaultValue));
+		
+		return parameterFieldPanel;
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		String actionCommand = e.getActionCommand();
+		Object actionSource = e.getSource();
+		
+		if(actionCommand.equals("Analyze")){
+			
+		}
+		else if(actionCommand.equals("Save result")) {
+			this.close();
+		}
+	}
+
+	private class ImageProcessing
+	{
+		ImageProcessing() {
+			
+		}
+	}//ImageProcessing inner class
 }
 
