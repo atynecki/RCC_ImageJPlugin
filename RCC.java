b8a19ec020bdd5d7e1dee5cc72da4849c0a6dc29
Anna <Anna@Ania_yoga>
2016-05-15 22:47:27 +0200

    image processing

--- RCC.java
+++ RCC.java
@@ -1,7 +1,10 @@
 import ij.*;
 import ij.process.*;
 import ij.gui.*;
+import ij.io.SaveDialog;
 import ij.plugin.frame.*;
+import ij.plugin.ImageCalculator;
+import ij.plugin.Duplicator;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -11,6 +14,8 @@ public class RCC extends PlugInFrame implements ActionListener {
 	private Panel panel;
 	private static Frame instance;
 	private ImagePlus image = new ImagePlus();
+	public static ImagePlus image1 = new ImagePlus();
+	public static ImagePlus image2 = new ImagePlus();
 	
 	private static int frame_width = 400;
 	private static int frame_height = 400;
@@ -43,7 +48,7 @@ public class RCC extends PlugInFrame implements ActionListener {
 		
 		/** GET ACTIVE IMAGE */
 		image = IJ.getImage();
-		
+				
 		GUI.center(this);
 		setVisible(true);
 	}
@@ -82,17 +87,41 @@ public class RCC extends PlugInFrame implements ActionListener {
 		Object actionSource = e.getSource();
 		
 		if(actionCommand.equals("Analyze")){
-			
+			ImageProcessing.ImageProcess(image);
+							
 		}
 		else if(actionCommand.equals("Save result")) {
+			SaveDialog saveImage = new SaveDialog("Save image...", "image", ".jpg");
+			
 			this.close();
 		}
 	}
 
-	private class ImageProcessing
+	private static class ImageProcessing
 	{
-		ImageProcessing() {
+		public ImageProcessing() {
+			}
+		
+		public static ImagePlus ImageProcess(ImagePlus img) {
+						
+			IJ.run(img, "8-bit", "");
+									
+			int sigma = 3; // do pobrania z aplikacji, wartosc do Gaussian Blur, jak bardzo ma byæ rozmyte
 			
+			for (int i = 0; i < sigma; i++)
+			{ 
+			  IJ.run(img, "Gaussian Blur...", "Sigma = 1"); 
+			}
+				
+				IJ.run(img, "Make Binary", "");
+				IJ.run(img, "Fill Holes", "");
+				IJ.run(img, "Watershed", "");
+				IJ.run(img, "Analyze Particles...", "size=0-infinity circularity=0.00-1.00 show=[Overlay Outlines] display record slice");
+				img.show();
+					
+			
+			
+			return img;
 		}
 	}//ImageProcessing inner class
 }
